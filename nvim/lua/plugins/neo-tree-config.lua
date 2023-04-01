3ef7ce8578a7afd765c67521f4dd3417a6a9c5a2
Ideles <pyjy@yahoo.com>
2023-04-01 14:48:49 +0800

    nvim: let latexindent use local settings; hide dotfiles in neotree

--- nvim/lua/plugins/neo-tree-config.lua
+++ nvim/lua/plugins/neo-tree-config.lua
@@ -1,14 +1,16 @@
 require("neo-tree").setup {
   filesystem = {
     filtered_items = {
-      hide_dotfiles = false,
+      hide_dotfiles = true,
       hide_gitignored = true,
-      hide_hidden = false, -- only works on Windows for hidden files/directories
+      hide_hidden = true, -- only works on Windows for hidden files/directories
       hide_by_name = {
         "node_modules",
         ".git",
         "__pycache__",
         "Pipfile.lock",
+        "yarn.lock",
+        "package-lock.json",
       },
     },
   },
