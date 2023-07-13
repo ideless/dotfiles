d18a74761367e4734d79f431e12a022fb4239034
ideless <pyjy@yahoo.com>
2023-07-13 10:26:33 +0800

    fix(nvim): fix and clear keymaps

--- nvim/lua/plugins/specs/ui/explorer.lua
+++ nvim/lua/plugins/specs/ui/explorer.lua
@@ -11,64 +11,68 @@ return {
       { "<Leader>e", ":NeoTreeFloatToggle<CR>", desc = "Toggle explorer" },
       { "<Leader>E", ":NeoTreeShowToggle<CR>", desc = "Toggle explorer (side)" },
     },
-    opts = {
-      sources = {
-        "filesystem",
-        "buffers",
-        "git_status",
-        "document_symbols",
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = true,
-          hide_gitignored = true,
-          hide_hidden = true, -- only works on Windows for hidden files/directories
-          hide_by_name = {
-            "node_modules",
-            ".git",
-            "__pycache__",
-            "Pipfile.lock",
-            "yarn.lock",
-            "package-lock.json",
+    opts = function()
+      local utils = require("utils")
+
+      return {
+        sources = {
+          "filesystem",
+          "buffers",
+          "git_status",
+          "document_symbols",
+        },
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = true,
+            hide_gitignored = true,
+            hide_hidden = true, -- only works on Windows for hidden files/directories
+            hide_by_name = {
+              "node_modules",
+              ".git",
+              "__pycache__",
+              "Pipfile.lock",
+              "yarn.lock",
+              "package-lock.json",
+            },
+          },
+          window = {
+            mappings = {
+              ["i"] = "show_fs_stat",
+            },
+          },
+          commands = {
+            show_fs_stat = function(state)
+              local node = state.tree:get_node()
+              local stat = vim.loop.fs_stat(node.path)
+              local str = ""
+              str = str .. string.format("Type: %s\n", stat.type)
+              str = str .. string.format("Size: %s\n", utils.format_size(stat.size))
+              str = str .. string.format("Time: %s\n", utils.format_time(stat.mtime.sec))
+              str = str .. string.format("Mode: %s\n", utils.format_mode(stat.mode, stat.type))
+              vim.notify(str)
+            end,
+          },
+        },
+        source_selector = {
+          winbar = true,
+          statusline = false,
+          sources = {
+            { source = "filesystem", display_name = " 󰉓 Files " },
+            -- { source = "buffers", display_name = " 󰈙 Buffers " },
+            { source = "git_status", display_name = " 󰊢 Git " },
+            { source = "document_symbols" },
           },
         },
         window = {
           mappings = {
-            ["i"] = "show_fs_stat",
+            ["<C-c>"] = "close_window",
+            ["<Space>"] = {
+              "toggle_node",
+              nowait = true,
+            },
           },
         },
-        commands = {
-          show_fs_stat = function(state)
-            local node = state.tree:get_node()
-            local stat = vim.loop.fs_stat(node.path)
-            local str = ""
-            str = str .. string.format("Type: %s\n", stat.type)
-            str = str .. string.format("Size: %s\n", utils.format_size(stat.size))
-            str = str .. string.format("Time: %s\n", utils.format_time(stat.mtime.sec))
-            str = str .. string.format("Mode: %s\n", utils.format_mode(stat.mode, stat.type))
-            vim.notify(str)
-          end,
-        },
-      },
-      source_selector = {
-        winbar = true,
-        statusline = false,
-        sources = {
-          { source = "filesystem", display_name = " 󰉓 Files " },
-          -- { source = "buffers", display_name = " 󰈙 Buffers " },
-          { source = "git_status", display_name = " 󰊢 Git " },
-          { source = "document_symbols" },
-        },
-      },
-      window = {
-        mappings = {
-          ["<C-c>"] = "close_window",
-          ["<Space>"] = {
-            "toggle_node",
-            nowait = true,
-          },
-        },
-      },
-    },
+      }
+    end,
   },
 }
