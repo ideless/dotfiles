fcab22041d1b922344ba7889d07b957afee690f6
Ideles <pyjy@yahoo.com>
2023-07-25 09:32:43 +0800

    feat(wezterm): random fonts

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,16 +7,25 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font("CodeNewRoman Nerd Font") -- Use Nerd Font 2.x.x, for example https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/CodeNewRoman.zip
+
+local my_fonts = {
+  wezterm.font("Operator Mono", { weight = "DemiLight" }),
+  wezterm.font("Code New Roman"),
+  wezterm.font("Fira Code"),
+  wezterm.font("JetBrains Mono"),
+}
+
+config.font = my_fonts[math.random(#my_fonts)]
+
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 config.hide_tab_bar_if_only_one_tab = true
 -- config.default_prog = {
@@ -24,56 +33,56 @@ config.hide_tab_bar_if_only_one_tab = true
 -- }
 config.color_scheme = "Andromeda"
 config.keys = {
-	{
-		key = "|",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "_",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "LeftArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.AdjustPaneSize({ "Left", 1 }),
-	},
-	{
-		key = "DownArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.AdjustPaneSize({ "Down", 1 }),
-	},
-	{
-		key = "UpArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.AdjustPaneSize({ "Up", 1 }),
-	},
-	{
-		key = "RightArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.AdjustPaneSize({ "Right", 1 }),
-	},
+  {
+    key = "|",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "_",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "h",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "j",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "k",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "l",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "LeftArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { "Left", 1 },
+  },
+  {
+    key = "DownArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { "Down", 1 },
+  },
+  {
+    key = "UpArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { "Up", 1 },
+  },
+  {
+    key = "RightArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { "Right", 1 },
+  },
 }
 -- config.debug_key_events = true
 
