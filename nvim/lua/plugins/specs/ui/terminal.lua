7bc1878b7e0e2fc89e662badd90c3553f790d3b0
Ideles <pyjy@yahoo.com>
2023-08-05 17:06:31 +0800

    fix(nvim): close gitui term without exiting

--- nvim/lua/plugins/specs/ui/terminal.lua
+++ nvim/lua/plugins/specs/ui/terminal.lua
@@ -3,14 +3,31 @@ return {
     "akinsho/toggleterm.nvim",
     keys = {
       -- <A-*> does not seem to work in which-key, put them here instead
-      { "<A-t>", ":ToggleTerm direction=float<CR>", mode = { "n" }, silent = true },
-      { "<A-t>", "<Cmd>ToggleTerm direction=float<CR>", mode = { "i", "x", "t" }, silent = true },
-      { "<A-T>\\", ":ToggleTerm direction=vertical<CR>", mode = { "n" }, silent = true },
-      { "<A-T>\\", "<Cmd>ToggleTerm direction=vertical<CR>", mode = { "i", "x", "t" }, silent = true },
-      { "<A-T>-", ":ToggleTerm direction=horizontal<CR>", mode = { "n" }, silent = true },
-      { "<A-T>-", "<Cmd>ToggleTerm direction=horizontal<CR>", mode = { "i", "x", "t" }, silent = true },
-      { "<A-T>s", "<Cmd>TermSelect<CR>", mode = { "n", "i", "x", "t" }, silent = true },
-      { "<A-T>n", ":ToggleTermSetName<CR>", mode = "n", silent = true },
+      { "<A-t>", mode = { "n", "i", "x", "t" }, silent = true },
+      { "<A-T>g", mode = { "n", "i", "x", "t" }, desc = "Gitui", silent = true },
+      {
+        "<A-T>=",
+        "<Cmd>ToggleTerm direction=float<CR>",
+        mode = { "n", "i", "x", "t" },
+        desc = "Toggle term (float)",
+        silent = true,
+      },
+      {
+        "<A-T>\\",
+        "<Cmd>ToggleTerm direction=vertical<CR>",
+        mode = { "n", "i", "x", "t" },
+        desc = "Toggle term (vertical)",
+        silent = true,
+      },
+      {
+        "<A-T>-",
+        "<Cmd>ToggleTerm direction=horizontal<CR>",
+        mode = { "n", "i", "x", "t" },
+        desc = "Toggle term (horizontal)",
+        silent = true,
+      },
+      { "<A-T>s", "<Cmd>TermSelect<CR>", mode = { "n", "i", "x", "t" }, desc = "Select term", silent = true },
+      { "<A-T>n", ":ToggleTermSetName<CR>", mode = "n", desc = "Set term name", silent = true },
       { "<Leader>gg", desc = "Gitui", silent = true },
     },
     opts = {
@@ -23,12 +40,6 @@ return {
       end,
       direction = "float",
       persist_mode = false,
-      winbar = {
-        enabled = true,
-        name_formatter = function(term) --  term: Terminal
-          return term.name
-        end,
-      },
     },
     config = function(_, opts)
       require("toggleterm").setup(opts)
@@ -36,8 +47,20 @@ return {
       local Terminal = require("toggleterm.terminal").Terminal
       local gitui = Terminal:new { cmd = "gitui", hidden = true }
 
-      vim.keymap.set("n", "<Leader>gg", function()
+      vim.keymap.set({ "n", "i", "x", "t" }, "<A-t>", function()
+        if gitui:is_open() then
+          gitui:close()
+        else
+          require("toggleterm").toggle()
+        end
+      end, { noremap = true, silent = true })
+
+      vim.keymap.set({ "n", "i", "x", "t" }, "<A-T>g", function()
         gitui:toggle()
+      end, { noremap = true, silent = true })
+
+      vim.keymap.set("n", "<Leader>gg", function()
+        gitui:open()
       end, { noremap = true, silent = true, desc = "Gitui" })
     end,
   },
