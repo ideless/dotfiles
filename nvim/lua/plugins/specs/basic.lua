e449ec433a2a0fca0d9d5db3d1d336c57e5eaa27
Ideles <pyjy@yahoo.com>
2023-09-04 19:26:37 +0800

    fix(nvim): crash on ultimate-autopair.nvim

--- nvim/lua/plugins/specs/basic.lua
+++ nvim/lua/plugins/specs/basic.lua
@@ -39,7 +39,9 @@ return {
   {
     "altermo/ultimate-autopair.nvim",
     event = { "InsertEnter", "CmdlineEnter" },
-    branch = "v0.6", -- nvim v0.9.1 stable would crash on it (https://github.com/altermo/ultimate-autopair.nvim/issues/39)
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    commit = "667d2304e8eb9ddbfa7f962528cfce0a5edcc163", -- https://github.com/altermo/ultimate-autopair.nvim/issues/39
+    opts = {},
   },
 
   -- guess indent
